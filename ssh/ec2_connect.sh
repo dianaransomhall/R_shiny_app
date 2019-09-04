ef3e31eca5e88bee77b2aac891d3054cbf833b51
Diana Hall <dianaransomhall@yahoo.com>
2019-09-03 19:05:12 -0700

    Initial commit

---
+++ ssh/ec2_connect.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# note the ip address will change after stop/start instance
+# go to aws.amazon.com, click on 'connect' and copy new addres
+ssh -i "keys/R_shiny_key_pair_Sept2019.pem" ubuntu@ec2-52-53-166-215.us-west-1.compute.amazonaws.com
+
